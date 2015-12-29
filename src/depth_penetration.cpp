aaa0fe35323e14b34a02c655ef2d7b6275716e7d
saurabdixit <saurabh.dxt21@gmail.com>
2015-12-29 15:49:39 -0800

    extenstion for depth penetration ready

--- src/depth_penetration.cpp
+++ src/depth_penetration.cpp
@@ -21,21 +21,29 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "valid_grasp_generator/GraspSnapshot.h"
-
-
+#include <iostream>
+#include <vector>
+#include <boost/python.hpp>
 using namespace OpenRAVE;
 using namespace std;
 
-void import_data(const valid_grasp_generator::GraspSnapshot::ConstPtr& grasp){
-    ROS_INFO("Got Grasp Extremes");
-}
-
-
-int main(int argc, char **argv)
+boost::python::list get_penetration_depth(string pointer)
 {
-    ros::init(argc,argv,"optimize_grasp");
-    ros::NodeHandle n;
-    ros::Subscriber grasp_extreme_sub = n.subscribe("grasp_extremes", 1000, import_data);
-    ros::spin();
-    return 0;
+    vector<double> vec(9,9);
+    cout << "Value of the pointer: " << pointer << endl;
+//code for getting penetration value from the openrave
+
+
+    typename std::vector<double>::iterator iter;
+    boost::python::list list;
+    for (iter = vec.begin(); iter != vec.end(); ++iter) {
+    	list.append(*iter);
+    }
+    return list;
 }
+
+BOOST_PYTHON_MODULE(libdepth_penetration){
+    using namespace boost::python;
+    def("get_penetration", get_penetration_depth);
+}
+
