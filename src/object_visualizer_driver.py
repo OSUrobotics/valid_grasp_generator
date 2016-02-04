dceb89b294885a0488170870e82057f849c0e8b6
saurabdixit <saurabh.dxt21@gmail.com>
2016-02-04 15:14:22 -0800

    Working on intermediate grasp generation

--- src/object_visualizer_driver.py
+++ src/object_visualizer_driver.py
@@ -56,24 +56,30 @@ def main2():
     print "Reading files from : ", folder_name 
     while not rospy.is_shutdown():
 	files = os.listdir(folder_name)
+        alpha_vector = np.arange(1,5,0.1)
         for csv_file in files:
+            interpolated_grasps = np.array([])
             similar_grasp_matrix = np.genfromtxt(csv_file,delimiter=',')
-            for vector in similar_grasp_matrix:
-                obj_num = vector[0]
-                sub_num = vector[1]
-                grasp_num = vector[2]
-                is_optimal = vector[3]
-                ext_opt_num = vector[4]
+            JointAngles = np.empty((2,10),np.float32)
+            HandTransformations = np.empty((2,4,4),np.float32)
+
+            for i in [0,1]:
+                obj_num = similar_grasp_matrix[i]
+                sub_num = similar_grasp_matrix[i]
+                grasp_num = similar_grasp_matrix[i]
+                is_optimal = similar_grasp_matrix[i]
+                ext_opt_num = similar_grasp_matrix[i]
                 if is_optimal == 1:
                     f = transform_path + "/" + "obj"+str(obj_num)+"_sub"+str(sub_num) + "/" + "obj"+str(obj_num)+"_sub"+str(sub_num)+"_grasp"+str(grasp_num)+"_optimal"+str(ext_opt_num)
                 else:
                     f = transform_path + "/" + "obj"+str(obj_num)+"_sub"+str(sub_num) + "/" + "obj"+str(obj_num)+"_sub"+str(sub_num)+"_grasp"+str(grasp_num)+"_extreme"+str(ext_opt_num)
 
-
 	        rospy.loginfo("Showing " + f)
 	        T_hand = np.genfromtxt(f+"_HandTransformation.txt",delimiter = ',')
                 T_obj = np.genfromtxt(f+"_ObjTransformation.txt",delimiter = ',')
                 joint_angles = np.genfromtxt(f+"_JointAngles.txt",delimiter = ',')[7:18]
+                JointAngles[i] = joint_angles
+                contact_names = np.gnefromtxt(f+"_ContactLinkNames.txt",delimiter = ',',dtype="|S")
 	        ctrl.reorient_hand(T_hand, T_obj)
                 ctrl.set_joint_angles(joint_angles)
 
